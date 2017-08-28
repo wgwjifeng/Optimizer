0cd89b4191b88ddb80927248784722919506151f
JiaJia <2894220@gmail.com>
2017-08-28 09:45:36 +0800

    First commit
    Signed-off-by: JiaJia <2894220@gmail.com>

---
+++ Optimizer/Optimizer.cpp
@@ -0,0 +1,16 @@
+﻿// Optimizer.cpp: 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "Optimizer.h"
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPTSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	return 0;
+}
